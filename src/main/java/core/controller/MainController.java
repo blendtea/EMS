1308ff405a24e54d42f0d2aaa11bb92332766e53
TSawazawa <68264750+TSawazawa@users.noreply.github.com>
2020-07-16 10:28:52 +0900

    7/16/1028

--- src/main/java/core/controller/MainController.java
+++ src/main/java/core/controller/MainController.java
@@ -1,14 +1,24 @@
 package core.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import core.model.User;
+import core.repository.UserRepository;
+
 @Controller
 public class MainController {
+    @Autowired
+    UserRepository userRepository;
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String main(Model model) {
-        return "main";
+    public String index(Model model) {
+        List<User> userlist=userRepository.findAll();
+        model.addAttribute("userlist", userlist);
+        return "index";
     }
 }
