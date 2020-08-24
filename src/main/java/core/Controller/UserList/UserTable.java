99c3a3a4eeed1ac4b0fed62cde5fc00bbecccd9a
Curse_Linux <54316870+blendtea@users.noreply.github.com>
2020-08-24 11:47:04 +0900

    Fixed package #UserList

---
+++ src/main/java/core/Controller/UserList/UserTable.java
@@ -0,0 +1,40 @@
+package core.Controller.UserList;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import core.Model.Profile;
+import core.Repository.UserProfileRepository;
+@RestController
+@Controller
+public class UserTable {
+	@Autowired
+    private UserProfileRepository profileRepository;
+
+	@GetMapping(value="/pages/UserList")
+	public ModelAndView UserList() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("pages/UserList");
+		return modelAndView;
+		}
+	//登録ユーザ一覧をリスト表示する
+    @GetMapping("/127.0.0.1")
+    public List<Profile> ID(Model model) {
+        List<Profile> list = profileRepository.findAll();
+        return list;
+    }
+    //サービスへ登録しているユーザーを一覧表示する
+    @GetMapping(value="/pages/Users")
+    public ModelAndView Users() {
+    	ModelAndView modelAndView = new ModelAndView();
+    	modelAndView.setViewName("pages/Users");
+    	return modelAndView;
+    }
+
+}
