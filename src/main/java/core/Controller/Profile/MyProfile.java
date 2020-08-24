5829d58103b84d47805c3f6394c3b7086d852035
Curse_Linux <54316870+blendtea@users.noreply.github.com>
2020-08-24 11:45:45 +0900

    Fixed package #Profile

--- src/main/java/core/Controller/Profile/MyProfile.java
+++ src/main/java/core/Controller/Profile/MyProfile.java
@@ -1,6 +1,4 @@
-package core.controller;
-
-import javax.transaction.Transactional;
+package core.Controller.Profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,39 +10,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import core.model.Profile;
-import core.model.User;
-import core.service.ProfileService;
-import core.service.UserService;
-@Transactional
+import core.Model.Profile;
+import core.Model.User;
+import core.Service.ProfileService;
+import core.Service.UserService;
+
 @Controller
 public class MyProfile {
 	@Autowired
     private UserService userService;
+	@Autowired
 	private ProfileService profileService;
 
 	@GetMapping(value="/pages/MyProfile")
-    public ModelAndView MyProfileCard(User user) {
+    public ModelAndView MyProfileCard(Profile profile, User identifier) {
 		ModelAndView mav = new ModelAndView();
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		user = userService.findUserByUserName(auth.getName());
-		//Object Mapping
-		mav.addObject("user", user);
-    	//Object data.String
-    	mav.addObject("FirstName",user.getFirstName());
-    	mav.addObject("LastName",user.getLastName());
-    	mav.addObject("fullName",user.getLastName() + user.getFirstName());
-//    	mav.addObject("employees_ID","社員番号"+"#"+ user.getUserName());
-    	mav.addObject("employees_ID",user.getUserName());
-    	mav.setViewName("pages/MyProfile");
-    	return mav;
+		identifier = userService.findUserByUserName(auth.getName());
+		mav.addObject("FirstName",identifier.getFirstName());
+    	mav.addObject("LastName",identifier.getLastName());
+    	mav.addObject("fullName",identifier.getLastName() + identifier.getFirstName());
+    	mav.addObject("employees_ID",identifier.getUserName());
+		mav.setViewName("pages/MyProfile");
+		return mav;
 	}
 	@GetMapping(value="/pages/iFunBox")
 	public ModelAndView TestField(Profile profile, User identifier) {
 		ModelAndView mav = new ModelAndView();
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		identifier = userService.findUserByUserName(auth.getName());
-		mav.addObject("profile", profile);
 		mav.addObject("FirstName",identifier.getFirstName());
     	mav.addObject("LastName",identifier.getLastName());
     	mav.addObject("fullName",identifier.getLastName() + identifier.getFirstName());
@@ -67,39 +61,13 @@ public class MyProfile {
 			System.out.println("User not found !");
 			}else {
 				profile.setId(finder.getId());
-//				profileService.save(profile);
+				profile.setUserName(finder.getUserName());
 				System.out.println(" ");System.out.println(" ");System.out.println(" ");System.out.println(" ");
 				System.out.println("finder CPU : " + profile.toString());
 				System.out.println(" ");System.out.println(" ");System.out.println(" ");System.out.println(" ");
-	            profileService.save(profile);
+				profileService.save(profile);
 				mav.setViewName("redirect:/pages/iFunBox");
-	        }
+				}
 		return mav;
 		}
-
-
-
-//	@PostMapping(value="/pages/MyProfile")
-//	@PreAuthorize("hasRole('USER')")
-//	public ModelAndView findByUserName(@ModelAttribute User user, BindingResult result) {
-//		ModelAndView mav = new ModelAndView();
-//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//		User finder = userService.findUserByUserName(auth.getName());
-//		if (result.hasErrors()) {
-//			mav.setViewName("redirect:/pages/UserList");
-//		}
-//		if (finder == null) {
-//			System.out.println("User not found !");
-//			}else {
-//				user.setId(finder.getId());
-//				user.setUserName(finder.getUserName());
-//				user.setPassword(finder.getPassword());
-//				user.setFirstName(finder.getFirstName());
-//				user.setLastName(finder.getLastName());
-//	            userService.save(user);
-////	            mav.addObject("user", auth.getName());
-//	            mav.setViewName("redirect:/pages/MyProfile");
-//	        }
-//		return mav;
-//		}
 	}
--- src/main/java/core/Controller/Profile/UserProfile.java
+++ src/main/java/core/Controller/Profile/UserProfile.java
@@ -1,25 +1,17 @@
-package core.controller;
-
-import java.util.List;
+package core.Controller.Profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import core.model.User;
-import core.repository.UserRepository;
-import core.service.UserService;
+import core.Model.User;
+import core.Service.UserService;
 
 @Controller
-@RestController
 public class UserProfile {
-	@Autowired
-    private UserRepository userRepository;
 	@Autowired
     private UserService userService;
 	@GetMapping(value="/pages/View")
@@ -32,22 +24,9 @@ public class UserProfile {
     	modelAndView.setViewName("pages/View");
     	return modelAndView;
     }
-    //登録ユーザ一覧をリスト表示する
-    @GetMapping("/127.0.0.1")
-    public List<User> ID(Model model) {
-        List<User> list = userRepository.findAll();
-        return list;
-    }
-    //サービスへ登録しているユーザーを一覧表示する
-    @GetMapping(value="/pages/Users")
-    public ModelAndView Users() {
-    	ModelAndView modelAndView = new ModelAndView();
-    	modelAndView.setViewName("pages/Users");	
-    	return modelAndView;
-    }
 }
 
-/* 
+/*
  * システムロケーション
  * Registration=>Login=>Home=>{Search}|{[Profile]}
  */
