4822bb81d7f531285bb762e21b0db05b2648f5dc
Curse_Linux <54316870+blendtea@users.noreply.github.com>
2020-09-02 13:53:48 +0900

    Update BeginningTheTerm.java

--- src/test/java/core/Service/BeginningTheTerm.java
+++ src/test/java/core/Service/BeginningTheTerm.java
@@ -17,10 +17,19 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 public class BeginningTheTerm {
 
+	//mockMvc Mock object for handling Http request/response without deploying to Tomcat server
 	@Autowired
 	private MockMvc mockMvc;
-
 	@Test
+	/*Specify view in get request and judge success or failure of http status
+	 Display request/response with andDo(print())
+	 ==============================================
+	 Be execute (perform)
+	Get request from "/login" (get)
+	Display results (andDo(print))
+	(for Result)Expect (andExpect)
+	is HTTP status is 200 (status().isOK())
+	 */
 		void HTTPisResponding() throws Exception {
     this.mockMvc.perform(get("/login")).andDo(print())
         .andExpect(status().isOk());
