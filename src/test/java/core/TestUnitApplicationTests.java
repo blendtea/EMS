ea119d4c055622395405e6a7a96c5d6a2565b166
Curse_Linux <54316870+blendtea@users.noreply.github.com>
2020-09-17 17:21:42 +0900

    post-commit
    mostly changed

--- src/test/java/core/TestUnitApplicationTests.java
+++ src/test/java/core/TestUnitApplicationTests.java
@@ -1,5 +1,6 @@
 package core;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ public class TestUnitApplicationTests {
 
 	@DisplayName("未設定")
 	@Test
+	@Disabled
 	public void contextLoads() {
 	}
 
